f57e01a53917ff1d4db89c09f53d0955c1cad6e5
sharmilasonavane24 <sharmila.sonavane24@gmail.com>
2018-10-19 11:50:37 +0200

    Added controllers on home page with css

--- PurchaseSchedule/ClientApp/src/TeamsConfigTab.js
+++ PurchaseSchedule/ClientApp/src/TeamsConfigTab.js
@@ -1,8 +1,10 @@
 ﻿import React from 'react';
-import { connectTeamsComponent, Panel, PanelBody } from 'msteams-ui-components-react';
+import { connectTeamsComponent, Panel, PanelBody,Input } from 'msteams-ui-components-react';
 import BuyerDropDown from './components/BuyerDropDown';
 import SupplierDropDown from './components/SupplierDropDown';
 import WorkspaceDropDown from './components/WorkspaceDropDown';
+import { inTeams } from './Utils';
+import microsoftTeams from '@microsoft/teams-js';
 
 class TeamsConfigTabInner extends React.Component {
     constructor(props) {
@@ -19,16 +21,43 @@ class TeamsConfigTabInner extends React.Component {
             selectedWorkspace: {
                workspacename: '',
                 workspaceid: ''
+            },
+            tabDetails:{
+                tabEntityId: '',
+                tabName: '',
+                isReady:false
             }
         };
         this.handleBuyerSelected = this.handleBuyerSelected.bind(this);
         this.handleSupplierSelected = this.handleSupplierSelected.bind(this);
         this.handleWorkspaceSelected = this.handleWorkspaceSelected.bind(this);
+        this.onTabNameChanged = this.onTabNameChanged.bind(this);
 
+        if (inTeams()) {
+            microsoftTeams.settings.registerOnSaveHandler((saveEvent) => {
+                let host = "https://" + window.location.host;
+                microsoftTeams.settings.setSettings({
+                    entityId: this.state.tabDetails.tabEntityId,
+                    Name: this.state.tabDetails.tabName,
+                    contentUrl: host + "/home/?theme={theme}&loginHint={loginHint}",
+                    suggestedDisplayName: "Purchase Schedule"
+                });
+                saveEvent.notifySuccess();
+            });
+        }
     }
 
     componentDidUpdate() {
-
+        if (inTeams()) {
+            if (this.state.selectedBuyer.buyername
+                && this.state.selectedSupplier.supplierid
+                && this.state.selectedWorkspace.workspaceid
+                && this.state.tabDetails.tabName) {
+                microsoftTeams.settings.setValidityState(true);
+            } else {
+                microsoftTeams.settings.setValidityState(false);
+            }
+        }
     }
 
     handleBuyerSelected(e, buyer) {
@@ -53,6 +82,17 @@ class TeamsConfigTabInner extends React.Component {
                 });
         }
     }
+    onTabNameChanged(e) {
+        if (e.target.value !== this.state.tabDetails.tabName ) {
+            this.setState(
+                {
+                    tabDetails: {
+                        tabName: e.target.value,
+                        tabEntityId: (e.target.value).replace(' ', '') + '_EntityId'
+                    }
+                });
+        }
+    }
     handleWorkspaceSelected(e, workspace) {
         if (workspace !== this.state.selectedWorkspace) {
             this.setState(
@@ -64,7 +104,11 @@ class TeamsConfigTabInner extends React.Component {
                 });
         }
     }
+
     render() {
+        const { context } = this.props;
+        const { rem, font } = context;
+        const { sizes, weights } = font;
         const styles = {
             panel:
             {
@@ -72,14 +116,26 @@ class TeamsConfigTabInner extends React.Component {
                 border: 0,
                 padding: 0,
                 margin: 0
+            },
+            input: {
+                paddingTop: rem(0.5),
+                width: '100%'
             }
         };
 
         return (
             <Panel style={styles.panel}>
                 <PanelBody>
+                    <Input
+                        autoFocus
+                        style={styles.input}
+                        placeholder="Tab name"
+                        label="Tab name"
+                        onChange={this.onTabNameChanged}
+                        required
+                        className={this.state.tabDetails.isReady?'hidden':''}
+                    />
                     <WorkspaceDropDown onClick={this.handleWorkspaceSelected} workspace={this.state.selectedWorkspace} />
-
                     <BuyerDropDown onClick={this.handleBuyerSelected} buyer={this.state.selectedBuyer} />
                     <SupplierDropDown onClick={this.handleSupplierSelected} supplier={this.state.selectedSupplier} />
 
--- PurchaseSchedule/ClientApp/src/components/Home.js
+++ PurchaseSchedule/ClientApp/src/components/Home.js
@@ -1,38 +1,109 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ToggleDisplay from 'react-toggle-display';
-import { IconButton,PrimaryButton, connectTeamsComponent } from 'msteams-ui-components-react';
-import { MSTeamsIconWeight,MSTeamsIconType  } from 'msteams-ui-icons-react';
-
+import { Toggle, IconButton, connectTeamsComponent, Table, TBody, Td, Tr, PrimaryButton, Dropdown } from 'msteams-ui-components-react';
+import { MSTeamsIconWeight, MSTeamsIconType } from 'msteams-ui-icons-react';
+import './Home.css';
 class HomeInner extends React.Component {
     constructor() {
         super();
-        this.state = { show: false };
+        this.state = {
+            show: false,
+            isPurchaseOrder: true,
+            isPlannedOrder: true,
+            isForecastSale: true,
+            isForecastLongTerm: true
+        };
     }
 
-    handleClick() {
+    handleShowClick() {
         this.setState({
             show: !this.state.show
         });
     }
-
+    handlePurchaseOrderToggle() {
+        this.setState({
+            isPurchaseOrder: !this.state.isPurchaseOrder
+        });
+    }
+    handlePlannedOrderToggle() {
+        this.setState({
+            isPlannedOrder: !this.state.isPlannedOrder
+        });
+    }
+    handleForecastSaleToggle() {
+        this.setState({
+            isForecastSale: !this.state.isForecastSale
+        });
+    }
+    handleForecastLongTermToggle() {
+        this.setState({
+            isForecastLongTerm: !this.state.isForecastLongTerm
+        });
+    }
     render() {
         return (
-            <div>
-                <p>
-                    <IconButton
-                        iconWeight={MSTeamsIconWeight.Regular}
-                        onClick={() => this.handleClick()}
-                        iconType={this.state.show ? MSTeamsIconType.ChevronDown : MSTeamsIconType.ChevronUp}
-                         />
-                </p>
-                <ToggleDisplay show={this.state.show}>
-                    I am rendered in a span (by default) and hidden with display:none when show is false.
-                </ToggleDisplay>
+            <div className="top-container">
 
-                <ToggleDisplay if={this.state.show} tag="section">
-                    I am rendered in a section and removed from the DOM when if is false.
-                </ToggleDisplay>
+                <IconButton
+                    iconWeight={MSTeamsIconWeight.Regular}
+                    onClick={() => this.handleShowClick()}
+                    iconType={this.state.show ? MSTeamsIconType.ChevronUp : MSTeamsIconType.ChevronDown}
+                />
+
+                <div className={this.state.show ? 'hidden' : ''}>
+
+                    <div className="row">
+                        <div className="column">
+                            <Table>
+                                <TBody>
+                                    <Tr>
+                                        <Td> Purchase orders:</Td>
+                                        <Td><Toggle autoFocus label='Purchase orders' checked={this.state.isPurchaseOrder} onToggle={() => this.handlePurchaseOrderToggle()} />
+                                        </Td>
+                                    
+                                        <Td> Planned orders:</Td>
+                                        <Td>
+                                            <Toggle  label='Purchase orders' checked={this.state.isPlannedOrder} onToggle={() => this.handlePlannedOrderToggle()} />
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>Forecast (sales):</Td>
+                                        <Td>
+                                            <Toggle  label='Purchase orders' checked={this.state.isForecastSale} onToggle={() => this.handleForecastSaleToggle()} />
+                                        </Td>
+                                    
+                                        <Td>Forecast (long Term): </Td>
+                                        <Td>
+                                            <Toggle  label='Purchase orders' checked={this.state.isForecastLongTerm} onToggle={() => this.handleForecastLongTermToggle()} />
+                                        </Td>
+                                    </Tr>
+                                </TBody>
+                            </Table>
+                            
+
+                        </div>
+                        <div className="column">
+                            <Dropdown
+                                label="Old version"
+                                mainButtonText="Old version"
+                                items={[
+                                    { text: 'option1', onClick: () => console.log('hello') },
+                                    { text: 'option2', onClick: () => console.log('hello') },
+                                    { text: 'option3', onClick: () => console.log('hello') },
+                                    { text: 'option4', onClick: () => console.log('hello') }
+                                ]}
+                            />
+                            <div>has been send to supplier</div>
+                        </div>
+                        <div className="column">
+                           <div> <PrimaryButton>Send report to supplier</PrimaryButton>
+                           <br /> <br /><PrimaryButton>Run report with filter</PrimaryButton></div>
+
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         );
     }
